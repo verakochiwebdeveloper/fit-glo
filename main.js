f36d5262142a45e671bc6cd6987a0cb1c931aba3
SIARHEI TSYKHUN~ <frontend4order@gmail.com>
2024-05-09 11:24:13 +0300

    add const in main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+const  title = 'title',
+screens = 'screens',
+screenPrice = 3,
+rollback = 'rollback',
+fullPrice = 5,
+adaptive = true;
+
+alert(' здесь могла быть ваша реклама');
+console.log(rollback)
