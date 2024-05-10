abaa6c47de614837e9b421db3cf56dab8c8c16fe
SIARHEI TSYKHUN~ <frontend4order@gmail.com>
2024-05-10 10:53:52 +0300

    Типы данных, операторы, методы и свойства

--- main.js
+++ main.js
@@ -1,9 +1,38 @@
-const  title = '';
-const  screens ='';
-const screenPrice = '' ;
-const rollback ='' ;
-const fullPrice = '' ;
-const constadaptive ='' ;
+// 1) Следующим переменным присвоить значения
+const  title = 'unknown project';
+const  screens ='Простые, Сложные, Интерактивные';
+const screenPrice = Math.ceil(Math.random()*70);
+const rollback = Math.ceil(Math.random()*100);
+const fullPrice = 150;
+const adaptive = true;
 
-alert(' здесь могла быть ваша реклама');
-console.log(rollback)
+
+// 2) Используя методы и свойства:
+
+//Вывести в консоль тип данных значений переменных title, fullPrice, adaptive;
+
+console.log(typeof title, typeof fullPrice, typeof adaptive );
+
+//Вывести в консоль длину строки из переменной screens
+
+console.log(screens.length);
+
+// Вывести в консоль “Стоимость верстки экранов (screenPrice) рублей/ долларов/гривен/юани” и “Стоимость разработки сайта (fullPrice) рублей/ долларов/гривен/юани”
+
+console.log('Стоимость верстки экранов ' + screenPrice +' рублей/ долларов/гривен/юани и ' + ' Стоимость разработки сайта' + fullPrice + 'рублей/ долларов/гривен/юани')
+
+// Привести строку screens к нижнему регистру и разбить строку на массив, вывести массив в консоль
+
+const screensLowerCase = (screens.toLowerCase());
+
+console.log(screensLowerCase);
+
+screensArray = screensLowerCase.split();
+
+console.log(screensArray);
+
+// Хотелось бы конечно в одну строку это все объеденить "screesArray = screensLowerCase.split(screens.toLowerCase())"
+
+//Вывести в консоль Процент отката посреднику за работу (fullPrice * (rollback/100))
+
+console.log(fullPrice * (rollback/100))
