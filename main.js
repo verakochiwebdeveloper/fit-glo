25114b8368c0e062682a17de4eaf153053308672
SIARHEI TSYKHUN~ <frontend4order@gmail.com>
2024-05-13 15:46:32 +0300

    main.js добавление циклов переменных

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
+const title = prompt("Как называется ваш проект");
+const screens = prompt(
+  'Какие типы экранов нужно разработать(пример: "Простые, Сложные, Интерактивные")'
+);
+const screenPrice = +prompt(
+  "Сколько будет стоить данная работа(пример: 12000)"
+);
+const rollback = Math.ceil(Math.random() * 100);
+const adaptive = confirm("Нужен ли адаптив на сайте");
+const service1 = prompt("Какой дополнительный тип услуги нужен?");
+const servicePrice1 = prompt(`Сколько это будет стоить? ${service1}`);
+const service2 = prompt("Какой дополнительный тип услуги нужен?");
+const servicePrice2 = prompt(`Сколько это будет стоить? ${service2}`);
+const agentBonus = 5000;
+const fullPrice =
+  Number(screenPrice) + Number(servicePrice1) + Number(servicePrice2);
+const servicePercentPrice = Number(fullPrice) - agentBonus;
 
-const  title = 'unknown project';
-const  screens ='Простые, Сложные, Интерактивные';
-const screenPrice = Math.ceil(Math.random()*70);
-const rollback = Math.ceil(Math.random()*100);
-const fullPrice = 150;
-const adaptive = true;
+console.log(fullPrice);
+console.log(Math.floor(servicePercentPrice));
 
-console.log(typeof title, typeof fullPrice, typeof adaptive );
+console.log(typeof title, typeof fullPrice, typeof adaptive);
 
 console.log(screens.length);
 
@@ -14,8 +27,29 @@ const cashAll = `Стоимость верстки экранов  ${screenPrice
 
 console.log(cashAll);
 
-const screensLowerCase = (screens.toLowerCase().split(','));
+const screensLowerCase = screens.toLowerCase().split(",");
 
 console.log(screensLowerCase);
 
-console.log(fullPrice * (rollback/100))
+console.log(fullPrice * (rollback / 100));
+
+switch (true) {
+  case fullPrice > 30000:
+    console.log('“Даем скидку в 10%”');
+    break;
+
+  case fullPrice >= 15000 && fullPrice <= 30000:
+    console.log("“Даем скидку в 5%”");
+    break;
+
+    case fullPrice <= 15000 && fullPrice > 0:
+    console.log('“Даем скидку в 5%”');
+    break;
+
+    case fullPrice == 0:
+    console.log('“Скидка не предусмотрена”');
+    break;
+
+    default:
+    console.log('что-то пошло не так');
+}
