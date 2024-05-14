27e563b3b69141fa7c25bf820ad727f33d2bb50a
SIARHEI TSYKHUN~ <frontend4order@gmail.com>
2024-05-14 16:38:46 +0300

    main.js fix

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-const title = prompt("Как называется ваш проект");
+const title = prompt("Как называется ваш проект?");
 const screens = prompt(
-  'Какие типы экранов нужно разработать(пример: "Простые, Сложные, Интерактивные")'
+  'Какие типы экранов нужно разработать?(пример: "Простые, Сложные, Интерактивные")'
 );
 const screenPrice = +prompt(
-  "Сколько будет стоить данная работа(пример: 12000)"
+  "Сколько будет стоить данная работа"
 );
 const rollback = Math.ceil(Math.random() * 100);
-const adaptive = confirm("Нужен ли адаптив на сайте");
+const adaptive = confirm("Нужен ли адаптив на сайте?");
 const service1 = prompt("Какой дополнительный тип услуги нужен?");
 const servicePrice1 = prompt(`Сколько это будет стоить? ${service1}`);
 const service2 = prompt("Какой дополнительный тип услуги нужен?");
 const servicePrice2 = prompt(`Сколько это будет стоить? ${service2}`);
-const agentBonus = 5000;
+
 const fullPrice =
   Number(screenPrice) + Number(servicePrice1) + Number(servicePrice2);
-const servicePercentPrice = Number(fullPrice) - agentBonus;
+const servicePercentPrice = fullPrice - rollback;
 
 console.log(fullPrice);
 console.log(Math.floor(servicePercentPrice));
@@ -23,7 +23,7 @@ console.log(typeof title, typeof fullPrice, typeof adaptive);
 
 console.log(screens.length);
 
-const cashAll = `Стоимость верстки экранов  ${screenPrice} рублей/ долларов/гривен/юани и  Стоимость разработки сайта ${fullPrice} рублей/ долларов/гривен/юани`;
+const cashAll = `Стоимость верстки экранов${screenPrice} рублей/ долларов/гривен/юани и Стоимость разработки сайта${fullPrice} рублей/ долларов/гривен/юани`;
 
 console.log(cashAll);
 
@@ -33,23 +33,26 @@ console.log(screensLowerCase);
 
 console.log(fullPrice * (rollback / 100));
 
-switch (true) {
-  case fullPrice > 30000:
+
+  if( fullPrice >= 30000) {
     console.log('“Даем скидку в 10%”');
-    break;
+    } else if (fullPrice >= 15000 && fullPrice <= 30000) {
+      console.log("“Даем скидку в 5%”");
+    } else if ( fullPrice <= 15000 && fullPrice > 0) {
+      console.log('“Даем скидку в 5%”');
+    } else if (fullPrice = 0) {
+      console.log('“Скидка не предусмотрена”');
+    } else {
+      console.log('что-то пошло не так');
+    }
 
-  case fullPrice >= 15000 && fullPrice <= 30000:
-    console.log("“Даем скидку в 5%”");
-    break;
+   
+    
 
-    case fullPrice <= 15000 && fullPrice > 0:
-    console.log('“Даем скидку в 5%”');
-    break;
+    
+    
+    
 
-    case fullPrice == 0:
-    console.log('“Скидка не предусмотрена”');
-    break;
+    
 
-    default:
-    console.log('что-то пошло не так');
-}
+  
