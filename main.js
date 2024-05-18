ea5523e840cc628756b881bdb51daa6ed3a8d573
SIARHEI TSYKHUN~ <frontend4order@gmail.com>
2024-05-18 13:29:21 +0300

    main.js fix

--- main.js
+++ main.js
@@ -7,16 +7,15 @@ const service1 = prompt("Какой дополнительный тип услу
 const servicePrice1 = +prompt(`Сколько это будет стоить? ${service1}`);
 const service2 = prompt("Какой дополнительный тип услуги нужен?");
 const servicePrice2 = +prompt(`Сколько это будет стоить? ${service2}`);
-
-const getTitle = function ( getTitleText) {
-  getTitleText = title.toLowerCase().split(",")
-
-  if (getTitleText[0] != '') {
-    return getTitleText.toLowerCase(0).toString 
-  } else {
-    return getTitleText[1].toLowerCase(0).toString 
-  }
-}
+// Первая идея была преобразовать title в массив через if проверить являеться ли первый элемент массива пустым если да то второй элемент массива сделать с большой буквы и вывести массив
+//Если Первый эмент масива не пустой то вывести первый элемент масива с большой + весь масив с маленькой.
+const getTitle = function (  ) {
+  let getTitletrim = title.trim();
+  const firstLetter = getTitletrim.charAt(0).toUpperCase();
+  const restOfTitle = getTitletrim.slice(1).toLowerCase()
+  return firstLetter + restOfTitle;
+  
+};
 
 console.log(getTitle())
 
@@ -42,43 +41,41 @@ const getAllServicePrices = function (servicePriceItem1, servicePriceItem2) {
 console.log(getAllServicePrices(servicePrice2, servicePrice1));
 
 function getFullPrice() {
-  const fullPrice2 = screenPrice + getAllServicePrices(servicePrice1, servicePrice2);
-  return fullPrice2;
+  const fullPrice = screenPrice + getAllServicePrices(servicePrice1, servicePrice2);
+  return fullPrice;
 }
 
 console.log(getFullPrice())
 
+const showTypeOf =  function(variable) {
+  return (variable, typeof variable)
+
+}
+
+console.log(showTypeOf(title));
+console.log(showTypeOf(screenPrice));
+console.log(showTypeOf(adaptive));
+
+const getServicePercentPrices = function () {
+  const servicePercentPrice = getFullPrice() - rollback;
+  return servicePercentPrice; 
+}
+
+ function getRollbackMessage () {
+  if (getFullPrice() >= 30000) {
+    return "Даем скидку в 10%"
+  } else 
+  if (getFullPrice() >= 15000 && fullPrice <= 30000) {
+    return "Даем скидку в 5%"
+  } else 
+  if (getFullPrice() <= 15000 && fullPrice >= 0) {
+    return "Скидка не предусмотрена"
+  } else {
+    return"что-то пошло не так"
+  }
+ }
 
 
-
-/*
-
-
-console.log(Math.ceil(servicePercentPrice));
-
-
-
-console.log(screens.length);
-
-const cashAll = `Стоимость верстки экранов${screenPrice} рублей/ долларов/гривен/юани и Стоимость разработки сайта${getFullPrice()} рублей/ долларов/гривен/юани`;
+ const cashAll = `Стоимость верстки экранов${screenPrice} рублей/ долларов/гривен/юани и Стоимость разработки сайта ${getFullPrice} рублей/ долларов/гривен/юани`;
 
 console.log(cashAll);
-
-const screensLowerCase = screens.toLowerCase().split(",");
-
-console.log(screensLowerCase);
-
-console.log(getFullPrice() * (rollback / 100));
-
-if (getFullPrice() >= 30000) {
-  console.log("“Даем скидку в 10%”");
-} else 
-if (getFullPrice() >= 15000 && fullPrice <= 30000) {
-  console.log("“Даем скидку в 5%”");
-} else 
-if (getFullPrice() <= 15000 && fullPrice >= 0) {
-  console.log("“Скидка не предусмотрена”");
-} else {
-  console.log("что-то пошло не так");
-}
-*/
