da291e53cfeee9950491e979a23e3f4a9d8d90a9
SIARHEI TSYKHUN~ <frontend4order@gmail.com>
2024-05-21 11:05:16 +0300

    main.js fixed func

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const title = prompt("Как называется ваш проект?");
+let title = prompt("Как называется ваш проект?");
 const screens = prompt('Какие типы экранов нужно разработать?','Простые, Сложные, Интерактивные');
 const screenPrice = +prompt("Сколько будет стоить данная работа");
 const rollback = Math.ceil(Math.random() * 100);
@@ -7,41 +7,25 @@ const service1 = prompt("Какой дополнительный тип услу
 const servicePrice1 = +prompt(`Сколько это будет стоить? ${service1}`);
 const service2 = prompt("Какой дополнительный тип услуги нужен?");
 const servicePrice2 = +prompt(`Сколько это будет стоить? ${service2}`);
-// Первая идея была преобразовать title в массив через if проверить являеться ли первый элемент массива пустым если да то второй элемент массива сделать с большой буквы и вывести массив
-//Если Первый эмент масива не пустой то вывести первый элемент масива с большой + весь масив с маленькой.
-const getTitle = function (  ) {
-  let getTitletrim = title.trim();
+
+const getTitle = function (titletext) {
+  let getTitletrim = titletext.trim();
   const firstLetter = getTitletrim.charAt(0).toUpperCase();
   const restOfTitle = getTitletrim.slice(1).toLowerCase()
   return firstLetter + restOfTitle;
-  
 };
 
-console.log(getTitle())
-
-/* первый вариант  решения с сумами
-  const getAllServicePrices = function () { 
-  const allServicePrices = servicePrice1 + servicePrice2; 
-  return allServicePrices; 
-} 
-console.log(getAllServicePrices()) 
- 
-function getFullPrice() { 
-  const fullPrice = screenPrice + getAllServicePrices(); 
-  return fullPrice; 
-} 
-*/
+title = getTitle(title);
 
 
-/* второй вариант решения */
-const getAllServicePrices = function (servicePriceItem1, servicePriceItem2) {
-  const allServicePrices = servicePriceItem1 + servicePriceItem2;
-  return allServicePrices;
+
+const getAllServicePrices = function (price1, price2) {
+  return price1 + price2;
 }
-console.log(getAllServicePrices(servicePrice2, servicePrice1));
+const servicePrices = getAllServicePrices(price1, price2);
 
 function getFullPrice() {
-  const fullPrice = screenPrice + getAllServicePrices(servicePrice1, servicePrice2);
+  const fullPrice = screenPrice + getAllServicePrices(price1, price2);
   return fullPrice;
 }
 
@@ -61,21 +45,23 @@ const getServicePercentPrices = function () {
   return servicePercentPrice; 
 }
 
+
+
  function getRollbackMessage () {
   if (getFullPrice() >= 30000) {
     return "Даем скидку в 10%"
-  } else 
+  } else {}
   if (getFullPrice() >= 15000 && fullPrice <= 30000) {
     return "Даем скидку в 5%"
   } else 
   if (getFullPrice() <= 15000 && fullPrice >= 0) {
     return "Скидка не предусмотрена"
   } else {
-    return"что-то пошло не так"
+    return "что-то пошло не так"
   }
  }
 
 
- const cashAll = `Стоимость верстки экранов${screenPrice} рублей/ долларов/гривен/юани и Стоимость разработки сайта ${getFullPrice} рублей/ долларов/гривен/юани`;
+ const cashAll = `Стоимость верстки экранов ${screenPrice} рублей/ долларов/гривен/юани и Стоимость разработки сайта ${getFullPrice} рублей/ долларов/гривен/юани`;
 
 console.log(cashAll);
