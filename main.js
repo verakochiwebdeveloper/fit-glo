199f08e8ea8590d9d7351a37613a64187f5e5124
SIARHEI TSYKHUN~ <frontend4order@gmail.com>
2024-05-30 17:29:34 +0300

    main.js fix

--- main.js
+++ main.js
@@ -1,53 +1,23 @@
-/*let title = prompt("Как называется ваш проект?");
-const screens = prompt(
-  "Какие типы экранов нужно разработать?",
-  "Простые, Сложные, Интерактивные"
-);
-let screenPrice;
-const rollback = Math.ceil(Math.random() * 100);
-let adaptive;
-const service1 = prompt("Какой дополнительный тип услуги нужен?");
-const servicePrice1 = +prompt(`Сколько это будет стоить? ${service1}`);
-const service2 = prompt("Какой дополнительный тип услуги нужен?");
-const servicePrice2 = +prompt(`Сколько это будет стоить? ${service2}`);
-
-const getTitle = function (titletext) {
-  let getTitletrim = titletext.trim();
-  const firstLetter = getTitletrim.charAt(0).toUpperCase();
-  const restOfTitle = getTitletrim.slice(1).toLowerCase();
-  return firstLetter + restOfTitle;
-};
-
-title = getTitle(title);
-*/
-let  appData = {
+let appData = {
   title: '',
   screens: '',
- screenPrice: 0,
- adaptive: true,
+  screenPrice: 0,
+  adaptive: true,
   rollback: Math.ceil(Math.random() * 100),
   AllServicePrices: 0,
   fullPrice: 0,
   servicePercentPrice: 0,
- service1: '',
- service2: '',
- 
+  service1: '',
+  service2: '',
 
-}
+  getTitle: function(titletext) {
+    let getTitletrim = titletext.trim();
+    const firstLetter = getTitletrim.charAt(0).toUpperCase();
+    const restOfTitle = getTitletrim.slice(1).toLowerCase();
+    return firstLetter + restOfTitle;
+  },
 
-let start = {
-  asking: functtion () {
-    appData.title = prompt("Как называется ваш проект?")
-    appData.screens = prompt("Какие типы экранов нужно разработать?","Простые, Сложные, Интерактивные"),
-  
-    do {
-      appdata.screenPrice = prompt( "Сколько будет стоить данная работа?")
-    } while(isNumber(appData.screenPrice))
-  
-      appData.adaptive("Нужен, ли адаптив на сайте")
-   }
-
-   getAllServicePrices: function getAllServicePrices (price1, price2) {
+  getAllServicePrices: function(price1, price2) {
     while (true) {
       if (isNaN(price1) || price1 === "" || price1 === null) {
         price1 = parseFloat(price1);
@@ -55,25 +25,27 @@ let start = {
       } else {
         alert("Пожалуйста, введите число.");
       }
-      while (true)
-        if (isNaN(price2) || price2 === "" || price2 === null) {
-          price2 = parseFloat(price2);
-          break;
-        } else {
-          alert("Пожалуйста, введите число.");
-        }
-      return price1 + price2;
     }
-  }
-
-  getFullPrice: function getFullPrice(price1, price2) {
+    while (true) {
+      if (isNaN(price2) || price2 === "" || price2 === null) {
+        price2 = parseFloat(price2);
+        break;
+      } else {
+        alert("Пожалуйста, введите число.");
+      }
+    }
     return price1 + price2;
-  }
-   isNumber: function isNumber(num) {
-    return !isNaN(parseFloat(num)) && isFinite(num)
-  }
+  },
 
-  getRollbackMessage: function getRollbackMessage (fullPrice) {
+  getFullPrice: function(price1, price2) {
+    return price1 + price2;
+  },
+
+  isNumber: function(num) {
+    return !isNaN(parseFloat(num)) && isFinite(num);
+  },
+
+  getRollbackMessage: function(fullPrice) {
     if (fullPrice >= 30000) {
       return "Даем скидку в 10%";
     } else if (fullPrice >= 15000) {
@@ -83,40 +55,32 @@ let start = {
     } else {
       return "что-то пошло не так";
     }
+  },
+
+  start: function() {
+    this.title = prompt("Как называется ваш проект?");
+    this.screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
+
+    do {
+      this.screenPrice = prompt("Сколько будет стоить данная работа?");
+    } while (this.isNumber(this.screenPrice));
+
+    this.adaptive = prompt("Нужен ли адаптив на сайте?");
+
+    const servicePrices = this.getAllServicePrices(servicePrice1, servicePrice2);
+
+    this.fullPrice = this.getFullPrice(this.screenPrice, servicePrices);
+
+    this.servicePercentPrice = this.fullPrice - this.rollback;
+
+    this.logger();
+  },
+
+  logger: function() {
+    for (let key in this) {
+      console.log(key + ": " + this[key]);
+    }
   }
-}
-
-
-
-
-
-const isNumber = function(num) {
-  return !isNaN(parseFloat(num)) && isFinite(num)
-}
-
-
-
-const servicePrices = getAllServicePrices(servicePrice1, servicePrice2);
-
-
-
-let fullPrice = getFullPrice(screenPrice, servicePrices);
-
-
-
-
-const getServicePercentPrices = function (price1, price2) {
-  return price1 - price2;
 };
 
-const servicePercentPrice = getServicePercentPrices(fullPrice, rollback);
-
-
-
-
-
-
-
-const cashAll = `Стоимость верстки экранов ${screenPrice} рублей/ долларов/гривен/юани и Стоимость разработки сайта ${fullPrice} рублей/ долларов/гривен/юани`;
-
-console.log(cashAll);
+appData.start();
